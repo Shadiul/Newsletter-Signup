60edc518935571d269ce35ecbaa5d965b61b5ad3
Shadiul <shadiul@users.noreply.github.com>
2020-04-03 07:35:44 +0600

    start script add

--- app.js
+++ app.js
@@ -76,5 +76,5 @@ app.post("/failure", function(req, res) {
 });
 
 app.listen(process.env.PORT || 3000, function() {
-  console.log("Server is running on port 3000");
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
